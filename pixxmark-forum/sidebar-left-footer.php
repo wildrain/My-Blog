b54c6d0bba9e6d0bb2a2cb8ab150b6d797bfff1a
wildrain <wild_rain@pixxmark.net>
2013-10-05 21:05:30 +0600

    small changed has beed made

--- pixxmark-forum/sidebar-left-footer.php
+++
@@ -1,5 +0,0 @@
-<?php if (!function_exists('dynamic_sidebar')|| !dynamic_sidebar('left-footer')): ?>
-
-	
-	
-<?php endif ?>
--- pixxmark-forum/sidebar-right-footer.php
+++
@@ -1,5 +0,0 @@
-<?php if (!function_exists('dynamic_sidebar')|| !dynamic_sidebar('right-footer')): ?>
-
-	
-	
-<?php endif ?>
